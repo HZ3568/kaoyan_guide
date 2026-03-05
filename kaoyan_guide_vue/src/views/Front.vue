0bc04cd34ad7a6db6fd8da574025d6ee30ba036c
hz <2194177433@qq.com>
2026-03-05 21:49:04 +0800

    隐藏上边栏

--- kaoyan_guide_vue/src/views/Front.vue
+++ kaoyan_guide_vue/src/views/Front.vue
@@ -14,12 +14,12 @@
           mode="horizontal"
         >
           <el-menu-item index="/front/home">首页</el-menu-item>
-          <el-menu-item index="/front/addApply">志愿填报</el-menu-item>
+          <!--<el-menu-item index="/front/addApply">志愿填报</el-menu-item>-->
           <el-menu-item index="/front/universityList">院校库</el-menu-item>
           <el-menu-item index="/front/interpretationsList"
             >专业解读</el-menu-item
           >
-          <el-menu-item index="/front/newsList">高考资讯</el-menu-item>
+          <!--<el-menu-item index="/front/newsList">高考资讯</el-menu-item>-->
           <el-menu-item index="/front/policysList">招生政策</el-menu-item>
           <el-menu-item index="/front/simulateExam">考场模拟</el-menu-item>
           <el-menu-item index="/front/consultCollege"
